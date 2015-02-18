3f17c176ec052dfe7703995040b9cb7f3ccd23ed
sophia-huynh <2575.00f@gmail.com>
2015-02-17 23:03:37 -0500

    Update models.py
    Untested thus far.

--- models.py
+++ models.py
@@ -29,4 +29,26 @@ class Project(models.Model):
     
     def __str__(self):
         return "{}: {}".format(self.pid, self.name)
+
+class UserReview(models.Model):
+    uid = models.ForeignKey('User')
+    reviewer = models.ForeignKey('User')
+    rid = models.ForeignKey('Rating')
+
+class ProjectReview(models.Model):
+    pid = models.ForeignKey('Project')
+    uid = models.ForeignKey('User')
+    rid = models.ForeignKey('Rating')
+
+class Rating(models.Model):
+    RATING_CHOICES = (
+        (1, 'Poor'),
+        (2, 'Below Average'),
+        (3, 'Average'),
+        (4, 'Above Average'),
+        (5, 'Outstanding'),
+        )
+    rid = models.AutoField(primary_key = True)
+    rating = models.IntegerField(choices = RATING_CHOICES, default = 3)
+    comment = models.TextField(max_length = 500)
     
